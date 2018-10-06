0a8bf6b25bdd373d5ff040663f3000f0b2e63500
yli313 <yitong-li@uiowa.edu>
2018-10-06 12:17:03 -0500

    revised reader

--- csvreader.py
+++ csvreader.py
@@ -1,20 +1,30 @@
 import csv
 def reader(filename):
     myReader = []
-    i = 0
+    i = -1
     with open (filename, newline = '') as csvfile:
         linereader = csv.reader(csvfile, delimiter = ' ', quotechar = '|')
-        while i < 20:
+        while i < 5:
+            i += 1
             for row in linereader:
                 a = ', '.join(row)
-                myReader.append(a)
-            i += 1
+                myReader.append(a.split(","))
     return(myReader[1:])
-            
+#    myReader = []
+#    i = -1
+#    with open (filename, newline = '') as csvfile:
+#        linereader = csv.reader(csvfile, delimiter = ' ', quotechar = '|')
+#        while i < 5:
+#            i += 1
+#            for row in linereader:
+#                a = ', '.join(row)
+#                myReader.append(a.split(",")
+#    return(myReader[1:])
 
 def readInDick(filename):
     Dick = dict()
     myList = reader(filename)
     for i in myList:
-        Dick[i[0:11]] = i[13:]
+        string = ''.join(i[1:])
+        Dick[i[0]] = string
     return (Dick)
