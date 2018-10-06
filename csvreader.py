ff151c884262095668bd2dc6af94624f60684103
yli313 <yitong-li@uiowa.edu>
2018-10-06 11:19:23 -0500

    untested reader

--- csvreader.py
+++ csvreader.py
@@ -1,1 +1,8 @@
-# HackySlackers2018
+import csv
+def reader(filename):
+    with open (filename, newline = '') as csvfile:
+        linereader = csv.reader(csvfile, delimeter = ' ', quotechar = '|')
+        i = 0
+        for row in linereader:
+            print(', '.join(row))
+
