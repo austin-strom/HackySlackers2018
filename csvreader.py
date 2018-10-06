88c473ec15c67724a7676fa9fd57f0ef4390c773
yli313 <yitong-li@uiowa.edu>
2018-10-06 12:01:55 -0500

    tested reader and read into dictionary

--- csvreader.py
+++ csvreader.py
@@ -1,8 +1,20 @@
 import csv
 def reader(filename):
+    myReader = []
+    i = 0
     with open (filename, newline = '') as csvfile:
-        linereader = csv.reader(csvfile, delimeter = ' ', quotechar = '|')
-        i = 0
-        for row in linereader:
-            print(', '.join(row))
+        linereader = csv.reader(csvfile, delimiter = ' ', quotechar = '|')
+        while i < 20:
+            for row in linereader:
+                a = ', '.join(row)
+                myReader.append(a)
+            i += 1
+    return(myReader[1:])
+            
 
+def readInDick(filename):
+    Dick = dict()
+    myList = reader(filename)
+    for i in myList:
+        Dick[i[0:11]] = i[13:]
+    return (Dick)
