e8248812abb2b357e4530860c96f63dc382c92e0
Austin Strom <austin-strom@uiowa.edu>
2018-10-07 06:58:43 -0500

    Added jars

--- CancerClassifier/src/RESTAPI/SQLQueryBuilder.java
+++ CancerClassifier/src/RESTAPI/SQLQueryBuilder.java
@@ -1,7 +1,30 @@
 package RESTAPI;
+import java.sql.*;
 
 public class SQLQueryBuilder {
-    public SQLQueryBuilder(){
+/*    public static String sendRequest() {
+        String requestURL;
 
-    }
+        //TODO: ANTONIO add driver name to pass in Class.forName();
+        Connection  conn = DriverManager.getConnection(requestURL, "root", "");
+
+        String query = "SELECT * FROM CancerData"; //TODO: ANTONIO INSERT TABLE NAME
+
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        ResultSet rs;
+        try {
+             rs = st.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        while(rs.next()){
+
+        }
+    }*/
 }
